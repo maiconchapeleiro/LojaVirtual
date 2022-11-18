f6509031c70a71db49082429517c3b45cf798be4
Maicon <maiconbrandao10@gmail.com>
2022-11-17 22:58:40 -0300

    iniciando

---
+++ backend/src/main/java/com/dev/backend/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.dev.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!";
+    }
+    
+}
